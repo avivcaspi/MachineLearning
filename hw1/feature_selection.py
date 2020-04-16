5b1534dec7e89331221229aab358dc4e2daa9dba
avivcaspi <avivcaspi@gmail.com>
2020-04-16 18:13:34 +0300

    little fix

--- hw1/feature_selection.py
+++ hw1/feature_selection.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from sklearn import metrics
 from sklearn.base import clone
-import random
 
 """
 Returns indices of features selected
@@ -11,33 +10,31 @@ def SFS(model, x_train: np.ndarray, y_train: np.ndarray, x_test: np.ndarray, y_t
     features = []
     features_left = list(range(x_train.shape[1]))
     best_accuracy = 0
-    best_feature = 0  # random initialization
+    best_feature = [0]  # random initialization
 
-    while best_feature is not None or len(features_left) == 0:
+    while len(best_feature) and len(features_left) > 0:
+        # Clone the model to avoid using fitted model again
         test_model = clone(model, safe=True)
-        best_feature = None
+        best_feature = []
         # going over all features left and finding best one
         for feature in features_left:
             current_features = sorted([*features, feature])
+            # take only current features from the data
             x_train_converted = x_train[:, current_features]
             x_test_converted = x_test[:, current_features]
+            # fit and predict
             test_model.fit(x_train_converted, y_train)
             y_pred = test_model.predict(x_test_converted)
             accuracy = metrics.accuracy_score(y_test, y_pred)
             if accuracy > best_accuracy:
                 best_accuracy = accuracy
-                best_feature = feature
-            elif accuracy == best_accuracy:
-                if isinstance(best_feature, list):
-                    best_feature.append(feature)
-                elif best_feature is None:
-                    best_feature = feature
-                else:
-                    best_feature = [feature, best_feature]
-        if best_feature is not None:
-            if isinstance(best_feature, list):
-                best_feature = random.choice(best_feature)
-            features.append(best_feature)
-            features_left.remove(best_feature)
+                best_feature = [feature]
+            elif accuracy == best_accuracy:  # if there are multiple features with same accuracy we save them
+                best_feature.append(feature)
+        # In case no improvement best_feature will be empty
+        if len(best_feature):
+            chosen_feature = np.random.choice(best_feature)
+            features.append(chosen_feature)
+            features_left.remove(chosen_feature)
     return sorted(features)
 
