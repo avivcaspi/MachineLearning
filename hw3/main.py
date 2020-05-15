525ac784a3e7f0e9b3b10c5b3090b67b4ce1b29d
avivcaspi <avivcaspi@gmail.com>
2020-05-15 13:24:06 +0300

    added section 1-5 in the mandatory part

--- hw3/main.py
+++ hw3/main.py
@@ -1,83 +1,145 @@
-from data_preperation import *
-from hw2.test import test_accuracy
+import pandas as pd
+import numpy as np
+from data_preperation import split_label_from_data
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
 
-selected_features = ['Yearly_IncomeK', 'Number_of_differnt_parties_voted_for', 'Political_interest_Total_Score',
-                     'Avg_Satisfaction_with_previous_vote', 'Avg_monthly_income_all_years', 'Most_Important_Issue',
-                     'Overall_happiness_score', 'Avg_size_per_room', 'Weighted_education_rank']
-onehot_nominal_features = ['Most_Important_Issue']
-features_with_negative = ['Avg_monthly_income_all_years']
 
-uniform_features = ['Yearly_IncomeK']
-normal_features = ['Number_of_differnt_parties_voted_for', 'Weighted_education_rank', 'Overall_happiness_score',
-                   'Avg_size_per_room', 'Avg_Satisfaction_with_previous_vote', 'Avg_monthly_income_all_years',
-                   'Political_interest_Total_Score']
+def temp():
+    """kf = KFold(n_splits=n_splits)
 
-discrete_features = ['Number_of_differnt_parties_voted_for']
-continuous_features = ['Yearly_IncomeK', 'Avg_size_per_room', 'Avg_Satisfaction_with_previous_vote',
-                       'Weighted_education_rank', 'Avg_monthly_income_all_years', 'Political_interest_Total_Score',
-                       'Overall_happiness_score']
-numerical_features = discrete_features + continuous_features
+        X_train = X_train.to_numpy()
+        y_train = y_train.to_numpy()
+        # finding best k for KNN classifier and n_estimators for RandomForestClassifier
+        knn_results = dict()
+        forest_results = dict()
+        for k, n_estimators in zip(range(1, 7), range(40, 160, 20)):
+            knn_accuracy = 0
+            forest_accuracy = 0
+            for i, (train_index, test_index) in enumerate(kf.split(X_train)):
+                KNN_classifier = KNeighborsClassifier(n_neighbors=k)
+                KNN_classifier.fit(X_train[train_index], y_train[train_index])
+                y_pred = KNN_classifier.predict(X_train[test_index])
+                knn_accuracy += accuracy_score(y_train[test_index], y_pred)
 
-if __name__ == '__main__':
-    load = True
-    if load:
-        XY_train = pd.read_csv('train_transformed.csv', index_col=0, header=0)
-        XY_val = pd.read_csv('val_transformed.csv', index_col=0, header=0)
-        XY_test = pd.read_csv('test_transformed.csv', index_col=0, header=0)
+                forest = RandomForestClassifier(n_estimators=n_estimators, min_samples_split=20, random_state=1)
+                forest.fit(X_train[train_index], y_train[train_index])
+                y_pred = forest.predict(X_train[test_index])
+                forest_accuracy += accuracy_score(y_train[test_index], y_pred)
 
-    else:
-        # Load data
-        df = pd.read_csv('ElectionsData.csv', header=0)
-        df = df[['Vote'] + selected_features]
+            knn_accuracy /= n_splits
+            knn_results[k] = knn_accuracy
+            forest_accuracy /= n_splits
+            forest_results[n_estimators] = forest_accuracy
+            print(f'k = {k} knn accuracy = {knn_accuracy}')
+            print(f'n_estimators = {n_estimators} forest accuracy = {forest_accuracy}')
+        best_k = max(knn_results, key=knn_results.get)
+        best_n_estimators = max(forest_results, key=forest_results.get)
+        print(f'best k is {best_k}')
+        print(f'best n_estimators is {best_n_estimators}')
+        # TODO run CV to find min sample split
+        best_knn = KNeighborsClassifier(best_k)
+        best_forest = RandomForestClassifier(best_n_estimators, min_samples_split=20, random_state=1)
+        best_knn.fit(X_train, y_train)
+        best_forest.fit(X_train, y_train)
+        return best_knn, best_forest"""
 
-        # Split data
-        train_indices, val_indices, test_indices = split_data(df, test_size=0.15, val_size=0.15)
-        df.loc[train_indices, :].to_csv('train_original.csv')
-        df.loc[val_indices, :].to_csv('val_original.csv')
-        df.loc[test_indices, :].to_csv('test_original.csv')
 
-        # Convert nominal features to onehot
-        df = convert_to_onehot(df, onehot_nominal_features)
+# Receive dict with classifiers and params with optional values, and finds the best combination of params values for
+# each classifier, it assumes each classifier has at most 2 params
+def find_best_params_CV(XY_train: pd.DataFrame, classifiers_params_dict: dict):
+    X_train, y_train = split_label_from_data(XY_train)
+    X_train = X_train.to_numpy()
+    y_train = y_train.to_numpy()
+    n_splits = 5
 
-        # Convert to categorical type
-        df = convert_to_categorical(df)
+    for classifier, params in classifiers_params_dict.items():
+        random_state = False
+        if 'random_state' in params.keys():
+            random_state = params['random_state']
+            params.pop('random_state')
+        if len(params) == 2:
+            param_name_1 = list(params.keys())[0]
+            values_1 = params[param_name_1]
+            param_name_2 = list(params.keys())[1]
+            values_2 = params[param_name_2]
+            best_score = 0
+            best_values = None
+            for value_1 in values_1:
+                for value_2 in values_2:
+                    params_dict = {param_name_1: value_1, param_name_2: value_2}
+                    if random_state:
+                        params_dict['random_state'] = random_state
+                    clf = classifier(**params_dict)
+                    score = np.mean(cross_val_score(clf, X_train, y_train, scoring='accuracy', cv=n_splits))
+                    if score > best_score:
+                        best_values = {param_name_1: value_1, param_name_2: value_2}
+                        best_score = score
+        elif len(params) == 1:
+            param_name_1 = list(params.keys())[0]
+            values_1 = params[param_name_1]
+            best_score = 0
+            best_values = None
+            for value_1 in values_1:
+                params_dict = {param_name_1: value_1}
+                if random_state:
+                    params_dict['random_state'] = random_state
+                clf = classifier(**params_dict)
+                score = np.mean(cross_val_score(clf, X_train, y_train, scoring='accuracy', cv=n_splits))
+                if score > best_score:
+                    best_values = {param_name_1: value_1}
+                    best_score = score
+        else:
+            continue
+        for param_name in params.keys():
+            classifiers_params_dict[classifier][param_name] = best_values[param_name]
+    return classifiers_params_dict
 
-        # Change negative values to positive
-        df = abs_negative(df, features_with_negative)
 
-        # Create sets
-        XY_train = df.loc[train_indices, :]
-        XY_val = df.loc[val_indices, :]
-        XY_test = df.loc[test_indices, :]
-
-        # Remove outlier from train set
-        print('Outlier removing')
-        print(f'Number of nan before: {XY_train.isnull().values.sum()}')
-        outlier = Outlier(XY_train, numerical_features)
-        XY_train = outlier.remove_outlier(XY_train, 4.5)
-        print(f'Number of nan after: {XY_train.isnull().values.sum()}')
-
-        # Imputation
-        print('Imputation')
-        print(f'Number of nan before: {XY_train.isnull().values.sum()}')
-        imputation = Imputation(XY_train, numerical_features, [])
-        XY_train = imputation.impute_train(XY_train)
-        print(f'Number of nan after: {XY_train.isnull().values.sum()}')
-
-        print(f'Number of nan before: {XY_test.isnull().values.sum() + XY_val.isnull().values.sum()}')
-        XY_test, XY_val = imputation.impute_test_val(XY_test, XY_val)
-        print(f'Number of nan after: {XY_test.isnull().values.sum() + XY_val.isnull().values.sum()}')
-
-        # Scaling
-        XY_train, XY_val, XY_test = scale_all(XY_train, XY_val, XY_test, uniform_features, normal_features)
-
-        XY_train.to_csv('train_transformed.csv')
-        XY_val.to_csv('val_transformed.csv')
-        XY_test.to_csv('test_transformed.csv')
-
+def train_and_evaluate(classifiers_params_dict: dict, XY_train: pd.DataFrame, XY_val: pd.DataFrame, score_function=accuracy_score):
     X_train, y_train = split_label_from_data(XY_train)
     X_val, y_val = split_label_from_data(XY_val)
-    X_test, y_test = split_label_from_data(XY_test)
-    print(test_accuracy(X_train, y_train, X_val, y_val))
+    results = dict()
+    for classifier, params in classifiers_params_dict.items():
+        clf = classifier(**params)
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_val)
+        score = score_function(y_val, y_pred)
+        results[classifier] = score
+    return results
 
 
+def train_best_classifier(XY_train: pd.DataFrame, classifier, params):
+    X_train, y_train = split_label_from_data(XY_train)
+    clf = classifier(**params)
+    clf.fit(X_train, y_train)
+    return clf
+
+
+def main():
+    XY_train = pd.read_csv('train_transformed.csv', index_col=0, header=0)
+    XY_val = pd.read_csv('val_transformed.csv', index_col=0, header=0)
+    XY_test = pd.read_csv('test_transformed.csv', index_col=0, header=0)
+    classifiers_params_dict = {RandomForestClassifier: {'n_estimators': list(range(60, 100, 20)),
+                                                        'min_samples_split': list(range(2, 10, 4)),
+                                                        'random_state': 2},
+                               KNeighborsClassifier: {'n_neighbors': list(range(1, 7))},
+                               SVC: {'kernel': ['linear', 'poly', 'rbf', 'sigmoid']},
+                               DecisionTreeClassifier: {'min_samples_split': list(range(2, 10, 4))},
+                               SGDClassifier: {'loss': ['hinge', 'log', 'modified_huber', 'squared_hinge', 'perceptron']},
+                               GaussianNB: {}}
+    classifiers_params_dict = find_best_params_CV(XY_train, classifiers_params_dict)
+    results = train_and_evaluate(classifiers_params_dict, XY_train, XY_val)
+    # TODO merge train and val data sets and use train best classifier to train and then predict what they asked in section 6
+    print(results, classifiers_params_dict)
+
+
+if __name__ == '__main__':
+    main()
---
+++ hw3/preprocessing.py
@@ -0,0 +1,83 @@
+from data_preperation import *
+from hw2.test import test_accuracy
+
+selected_features = ['Yearly_IncomeK', 'Number_of_differnt_parties_voted_for', 'Political_interest_Total_Score',
+                     'Avg_Satisfaction_with_previous_vote', 'Avg_monthly_income_all_years', 'Most_Important_Issue',
+                     'Overall_happiness_score', 'Avg_size_per_room', 'Weighted_education_rank']
+onehot_nominal_features = ['Most_Important_Issue']
+features_with_negative = ['Avg_monthly_income_all_years']
+
+uniform_features = ['Yearly_IncomeK']
+normal_features = ['Number_of_differnt_parties_voted_for', 'Weighted_education_rank', 'Overall_happiness_score',
+                   'Avg_size_per_room', 'Avg_Satisfaction_with_previous_vote', 'Avg_monthly_income_all_years',
+                   'Political_interest_Total_Score']
+
+discrete_features = ['Number_of_differnt_parties_voted_for']
+continuous_features = ['Yearly_IncomeK', 'Avg_size_per_room', 'Avg_Satisfaction_with_previous_vote',
+                       'Weighted_education_rank', 'Avg_monthly_income_all_years', 'Political_interest_Total_Score',
+                       'Overall_happiness_score']
+numerical_features = discrete_features + continuous_features
+
+if __name__ == '__main__':
+    load = True
+    if load:
+        XY_train = pd.read_csv('train_transformed.csv', index_col=0, header=0)
+        XY_val = pd.read_csv('val_transformed.csv', index_col=0, header=0)
+        XY_test = pd.read_csv('test_transformed.csv', index_col=0, header=0)
+
+    else:
+        # Load data
+        df = pd.read_csv('ElectionsData.csv', header=0)
+        df = df[['Vote'] + selected_features]
+
+        # Split data
+        train_indices, val_indices, test_indices = split_data(df, test_size=0.15, val_size=0.15)
+        df.loc[train_indices, :].to_csv('train_original.csv')
+        df.loc[val_indices, :].to_csv('val_original.csv')
+        df.loc[test_indices, :].to_csv('test_original.csv')
+
+        # Convert nominal features to onehot
+        df = convert_to_onehot(df, onehot_nominal_features)
+
+        # Convert to categorical type
+        df = convert_to_categorical(df)
+
+        # Change negative values to positive
+        df = abs_negative(df, features_with_negative)
+
+        # Create sets
+        XY_train = df.loc[train_indices, :]
+        XY_val = df.loc[val_indices, :]
+        XY_test = df.loc[test_indices, :]
+
+        # Remove outlier from train set
+        print('Outlier removing')
+        print(f'Number of nan before: {XY_train.isnull().values.sum()}')
+        outlier = Outlier(XY_train, numerical_features)
+        XY_train = outlier.remove_outlier(XY_train, 4.5)
+        print(f'Number of nan after: {XY_train.isnull().values.sum()}')
+
+        # Imputation
+        print('Imputation')
+        print(f'Number of nan before: {XY_train.isnull().values.sum()}')
+        imputation = Imputation(XY_train, numerical_features, [])
+        XY_train = imputation.impute_train(XY_train)
+        print(f'Number of nan after: {XY_train.isnull().values.sum()}')
+
+        print(f'Number of nan before: {XY_test.isnull().values.sum() + XY_val.isnull().values.sum()}')
+        XY_test, XY_val = imputation.impute_test_val(XY_test, XY_val)
+        print(f'Number of nan after: {XY_test.isnull().values.sum() + XY_val.isnull().values.sum()}')
+
+        # Scaling
+        XY_train, XY_val, XY_test = scale_all(XY_train, XY_val, XY_test, uniform_features, normal_features)
+
+        XY_train.to_csv('train_transformed.csv')
+        XY_val.to_csv('val_transformed.csv')
+        XY_test.to_csv('test_transformed.csv')
+
+    X_train, y_train = split_label_from_data(XY_train)
+    X_val, y_val = split_label_from_data(XY_val)
+    X_test, y_test = split_label_from_data(XY_test)
+    print(test_accuracy(X_train, y_train, X_val, y_val))
+
+
